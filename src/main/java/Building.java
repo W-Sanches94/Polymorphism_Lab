deb1c1bb2c881a9d5cf7896b7541e4dcbf54c232
W-Sanches94 <117460587+W-Sanches94@users.noreply.github.com>
2023-01-30 14:47:59 +0000

    Created Abstract Class for Building

--- src/main/java/Building.java
+++ src/main/java/Building.java
@@ -1,4 +1,41 @@
+import java.time.LocalDate;
+
 public abstract class Building {
 
+    protected String name;
+    protected int numberOfRooms;
+    protected LocalDate dateOfConstruction;
 
+    public Building (String name, int numberOfRooms, LocalDate dateOfConstruction){
+        this.name = name;
+        this.numberOfRooms = numberOfRooms;
+        this.dateOfConstruction = dateOfConstruction;
+    }
+
+    // GETTERS & SETTERS
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public void setNumberOfRooms(int numberOfRooms) {
+        this.numberOfRooms = numberOfRooms;
+    }
+
+    public LocalDate getDateOfConstruction() {
+        return dateOfConstruction;
+    }
+
+    public void setDateOfConstruction(LocalDate dateOfConstruction) {
+        this.dateOfConstruction = dateOfConstruction;
+    }
 }
